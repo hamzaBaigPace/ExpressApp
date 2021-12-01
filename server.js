c42e01c073874572eb5d1157e976e6e06ac4ea3b
hamzaBaigPace <hamzabaig@pace-tech.com>
2021-12-01 17:34:35 +0500

    Changes for code style

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT=3004;
 
 app.set("view engine","ejs");
 
-app.get("/",(req,res)=>{
+app.get("/",(req,res) => {
     console.log("Done");
    res.render("index",{name:"Hamza Baig"});
 });
@@ -30,26 +30,26 @@ app.use("/portfolio",portfolioRoutes);
 app.use("/blog",blogRoutes);
 app.use("/contact",contactRoutes);
 
-app.get("/hello",(req,res)=>{
-    res.send("<h1>Hello From the other side</h1>")
+app.get("/hello",(req,res) => {
+    res.send("<h1>Hello From the other side</h1>");
 });
 
 
-app.post("/blogPost",(req,res)=>{
-    res.json({status:"Success",data:[{name:"Hamza Baig",age:23},{name:"Haider Ali",age:31}]})
+app.post("/blogPost",(req,res) => {
+    res.json({status:"Success",data:[{name:"Hamza Baig",age:23},{name:"Haider Ali",age:31}]});
 });
 
-app.get("/downloadServer",(req,res)=>{
+app.get("/downloadServer",(req,res) => {
     res.download("server.js");
 });
 
-app.get("*",(req,res)=>{
+app.get("*",(req,res) => {
     console.log("PAGE NOT FOUND");
-    res.send("You must be lost!")
+    res.send("You must be lost!");
 });
 
 
 
-app.listen(PORT,()=>{
-    console.log(`Server listening at ${PORT}`)
+app.listen(PORT,() => {
+    console.log(`Server listening at ${PORT}`);
 })
